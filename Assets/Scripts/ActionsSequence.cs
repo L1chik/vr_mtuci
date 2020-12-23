19fb053cd996e0479499a331f512de13a2690928
Пушкарев Павел Сергеевич <ppushkarev@phoenixit.ru>
2020-12-23 23:49:15 +0300

    Initial commit

---
+++ Assets/Scripts/ActionsSequence.cs
@@ -0,0 +1,22 @@
+﻿public static class ActionsSequence
+{
+    public struct Action
+    {
+        public Action(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public string Name;
+        public string Description;
+    }
+
+    public static Action[] Actions =
+    {
+        new Action("Button1Pressed", "Нажми 'Button 1'"),
+        new Action("Button2Pressed", "Теперь нажми 'Button 2'"),
+        new Action("Button3Pressed", "Молодец! А теперь нажми 'Button 3'"),
+        new Action("Button4Pressed", "И, наконец, нажми 'Button 4'")
+    };
+}
---
+++ Assets/Scripts/ConsistencyTracker.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class ConsistencyTracker : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textOutput;
+
+    private UnityAction<string> _onAction;
+    private int _currentActionInd;
+
+    private void Start()
+    {
+        _onAction += CheckConsistency;
+
+        ShowCurrentActionDescription();
+    }
+
+    private void ShowCurrentActionDescription()
+    {
+        textOutput.text = ActionsSequence.Actions[_currentActionInd].Description;
+    }
+
+    private void CheckConsistency(string actionName)
+    {
+        if (ActionsSequence.Actions[_currentActionInd].Name == actionName)
+        {
+            if (_currentActionInd + 1 >= ActionsSequence.Actions.Length) return;
+            _currentActionInd++;
+            ShowCurrentActionDescription();
+        }
+        else
+        {
+            StartCoroutine(ShowErrorForNSeconds(2));
+        }
+    }
+
+    private IEnumerator ShowErrorForNSeconds(int n)
+    {
+        textOutput.text = "Не правильно!";
+
+        yield return new WaitForSeconds(n);
+
+        ShowCurrentActionDescription();
+    }
+
+    public void InvokeOnAction(string actionName)
+    {
+        _onAction.Invoke(actionName);
+    }
+}
