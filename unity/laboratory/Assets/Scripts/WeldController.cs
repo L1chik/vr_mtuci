63d6cced120e21c7a03bf6ff97b7051168007f2d
Пушкарев Павел Сергеевич <ppushkarev@phoenixit.ru>
2021-02-02 13:30:22 +0300

    VR_19: Update toolbox. Fix snap zones. Fix weld script

---
+++ unity/laboratory/Assets/Scripts/WeldController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using BNG;
+using UnityEngine;
+
+public class WeldController : MonoBehaviour
+{
+    [SerializeField] private GameObject mainLid;
+    [SerializeField] private bool isLeftCableSnapped;
+    [SerializeField] private bool isRightCableSnapped;
+
+    private JointHelper _lidJointHelper;
+
+    private void Start()
+    {
+        _lidJointHelper = mainLid.GetComponent<JointHelper>();
+    }
+
+    public void StartWelding()
+    {
+        if (!isLeftCableSnapped || !isRightCableSnapped)
+        {
+            Debug.Log("Не все кабели на месте");
+            return;
+        }
+
+        LockLid();
+
+        // TODO: Start particles
+
+        StartCoroutine(Welding());
+    }
+
+    private IEnumerator Welding()
+    {
+        yield return new WaitForSeconds(2f);
+
+        // TODO: Stop particles
+
+        // TODO: Connect cable ends
+
+        UnlockLid();
+
+        Debug.Log("Done");
+    }
+
+    private void LockLid()
+    {
+        _lidJointHelper.LockXRotation = _lidJointHelper.LockYRotation = _lidJointHelper.LockZRotation = true;
+    }
+
+    private void UnlockLid()
+    {
+        _lidJointHelper.LockXRotation = _lidJointHelper.LockYRotation = _lidJointHelper.LockZRotation = false;
+    }
+}
